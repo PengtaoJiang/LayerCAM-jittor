8f08639f9f5a922917f37dd5765f5cae528281e9
PengtaoJiang <pengtaojiang@163.com>
2021-06-14 22:24:06 +0800

    .

--- test.py
+++ test.py
@@ -3,48 +3,33 @@
 import torch
 import torch.nn.functional as F
 import torchvision.models as models
+import argparse
 
 from utils import *
 from cam.layercam import *
 
-# alexnet
-alexnet = models.alexnet(pretrained=True).eval()
-alexnet_model_dict = dict(type='alexnet', arch=alexnet, layer_name='features_10',input_size=(224, 224))
-alexnet_layercam = LayerCAM(alexnet_model_dict)
+def get_arguments():
+    parser = argparse.ArgumentParser(description='The Pytorch code of LayerCAM')
+    parser.add_argument("--img_path", type=str, default='images/ILSVRC2012_val_00000476.JPEG', help='Path of test image')
+    parser.add_argument("--layer_id", type=list, default=[4,9,16,23,30], help='The cam generation layer')
 
-input_image = load_image('images/'+'ILSVRC2012_val_00002193.JPEG')
-input_ = apply_transforms(input_image)
-if torch.cuda.is_available():
-  input_ = input_.cuda()
-predicted_class = alexnet(input_).max(1)[-1]
+    return parser.parse_args()
 
-layercam_map = alexnet_layercam(input_)
-basic_visualize(input_.cpu().detach(), layercam_map.type(torch.FloatTensor).cpu(),save_path='alexnet.png')
 
-# vgg
-vgg = models.vgg16(pretrained=True).eval()
-vgg_model_dict = dict(type='vgg16', arch=vgg, layer_name='features_30',input_size=(224, 224))
-vgg_layercam = LayerCAM(vgg_model_dict)
+if __name__ == '__main__':
+    args = get_arguments()
 
-input_image = load_image('images/'+'ILSVRC2012_val_00002193.JPEG')
-input_ = apply_transforms(input_image)
-if torch.cuda.is_available():
-  input_ = input_.cuda()
-predicted_class = vgg(input_).max(1)[-1]
+    input_image = load_image(args.img_path)
+    input_ = apply_transforms(input_image)
+    if torch.cuda.is_available():
+      input_ = input_.cuda()
 
-layercam_map = vgg_layercam(input_)
-basic_visualize(input_.cpu().detach(), layercam_map.type(torch.FloatTensor).cpu(),save_path='vgg.png')
+    vgg = models.vgg16(pretrained=True).eval()
+    for i in range(len(args.layer_id)):
+        layer_name = 'features_' + str(args.layer_id[i])
+        vgg_model_dict = dict(type='vgg16', arch=vgg, layer_name=layer_name, input_size=(224, 224))
+        vgg_layercam = LayerCAM(vgg_model_dict)
+        predicted_class = vgg(input_).max(1)[-1]
 
-# resnet
-resnet = models.resnet18(pretrained=True).eval()
-resnet_model_dict = dict(type='resnet18', arch=resnet, layer_name='layer4',input_size=(224, 224))
-resnet_layercam = LayerCAM(resnet_model_dict)
-
-input_image = load_image('images/'+'ILSVRC2012_val_00002193.JPEG')
-input_ = apply_transforms(input_image)
-if torch.cuda.is_available():
-  input_ = input_.cuda()
-predicted_class = resnet(input_).max(1)[-1]
-
-layercam_map = resnet_layercam(input_)
-basic_visualize(input_.cpu().detach(), layercam_map.type(torch.FloatTensor).cpu(),save_path='resnet.png')
+        layercam_map = vgg_layercam(input_)
+        basic_visualize(input_.cpu().detach(), layercam_map.type(torch.FloatTensor).cpu(),save_path='./vis/stage_{}.png'.format(i+1))
